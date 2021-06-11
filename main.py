fd17478ed0e9dbebad440db8753ea7e3dbce9129
AoifeCarty <agcdunne@gmail.com>
2021-06-11 12:36:40 +0100

    Created Netflix

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-1+1
 import pandas as pd
-data=pd.read_csv("BankChurners.csv")
+data=pd.read_csv("netflix_titles.csv")
 
-print(data.info())
+updatedfile=data.fillna(data.mean())
+print(data.shape,updatedfile.shape)
 
-#didnt updated comments
+
+
